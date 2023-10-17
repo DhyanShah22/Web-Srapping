b1d93ee9d38fd941fb1767d89f22afe1781ed9fd
DhyanShah22 <dhyanshah229@gmail.com>
2023-10-18 00:28:53 +0530

    Jobs Real Web

--- Real-Website/main.py
+++ Real-Website/main.py
@@ -4,3 +4,23 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 #print(html_text)
 soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+#print(job)
+for job in jobs:
+
+    publish = job.find('span', class_='sim-posted').span.text
+    #print(publish)
+
+    if 'few' in publish:
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')  # repalce will replace white lines
+        #print(company_name)
+
+        skills_req = job.find('span', class_= 'srp-skills').text.replace(' ','')
+        #print(skills_req)
+
+        print(f''' 
+        Company Name: {company_name}
+        Required Skills: {skills_req}
+        ''')
+
+        print('')
